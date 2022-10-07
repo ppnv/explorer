13e43360df7620551c6f6718cd6404d2dbb520b9
Sergio | PPNV <85553781+sergiomateiko@users.noreply.github.com>
2022-10-07 15:35:21 +0300

    Update themeConfig.js

--- themeConfig.js
+++ themeConfig.js
@@ -9,7 +9,7 @@ export const $themeBreakpoints = {}
 // APP CONFIG
 export const $themeConfig = {
   app: {
-    appName: 'Explorer', // Will update name in navigation menu (Branding)
+    appName: 'PPNV EXP', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
     appLogoImage: '/logo.png', // Will update logo in navigation menu (Branding)
   },
